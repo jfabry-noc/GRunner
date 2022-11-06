e5c67e2f3d73ab4653876583860195da201ee455
John Fabry <jjfabry@pm.me>
2022-11-06 17:57:13 -0500

    Adds scoring.
    Adds score attribute and method for updating and displaying the score.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Background
+
+Python project to familiarize myself with basic game design. The game is written in Python using [pygame](https://www.pygame.org/news). The idea is to work on it for a month as part of the [GitHub Game Off](https://github.blog/2020-10-27-github-game-off-2020/). This will be filled out more as development continues.
+
+# Credits
+
+- [Sky and city background](https://opengameart.org/content/urban-landscape)
+
--- src/main.py
+++ src/main.py
@@ -4,10 +4,17 @@ import sys
 
 PLAYER_INTERVAL = 15
 BACKGROUND_INTERVAL = 5
+SCALE_INTERVAL = 0.003
 
 # Class for Garrett Prime.
 class GPrime(pygame.sprite.Sprite):
     def __init__(self, interval: int):
+        """
+        Sprite child class to represent Garrett Prime, the player.
+
+        Attributes:
+            interval (int): Movement interval that corresponds to key presses.
+        """
         super().__init__()
         self.interval = interval
         self.image = pygame.transform.rotozoom(pygame.image.load("static/g_base.gif").convert_alpha(), 0, 2.5)
@@ -35,10 +42,12 @@ class GRunner:
         pygame.init()
         pygame.display.set_caption("G Runner")
         self.tick_rate = 30
+        self.score = 0
         self.screen = pygame.display.set_mode((800, 400))
         self.clock = pygame.time.Clock()
         self.background_surf = pygame.image.load("static/city.png").convert()
         self.background_rect = self.background_surf.get_rect(topleft = (0, 0))
+        self.font = pygame.font.Font("fonts/Prompt-Medium.ttf", 40)
 
         # Define properties for how rapidly objects move.
         self.movement_interval = BACKGROUND_INTERVAL
@@ -52,18 +61,30 @@ class GRunner:
         """
         Adjusts the background image.
         """
-        self.background_rect = self.background_surf.get_rect(topleft = (self.background_rect.x - self.movement_interval, 0))
+        self.background_rect = self.background_surf.get_rect(topleft = (self.background_rect.x - (self.movement_interval + (self.movement_interval * self.movement_scale)), 0))
         if self.background_rect.right <= 800:
             self.background_rect.left = 0
         self.screen.blit(self.background_surf, self.background_rect)
 
+    def display_score(self, increase: int = 0):
+        """
+        Displays the score. If a number is passed, the score will be incremented
+        by that amount.
+
+        Args:
+            increase (int): Amount by which to increment the score.
+        """
+        self.score += increase
+        self.score_surf = self.font.render(f"Score: {self.score}", False, "#8F3A84")
+        self.score_rect = self.score_surf.get_rect(center = (400, 40))
+        self.screen.blit(self.score_surf, self.score_rect)
 
     def run(self) -> None:
         """
         Starts game execution.
         """
         while True:
-            self.movement_scale += 0.1
+            self.movement_scale += SCALE_INTERVAL
             print(f"Movement scale: {self.movement_scale}")
             pygame.display.update()
             for event in pygame.event.get():
@@ -73,6 +94,7 @@ class GRunner:
 
             # Render the background first.
             self.print_background()
+            self.display_score()
 
             # Draw the player.
             self.gprime.draw(self.screen)
